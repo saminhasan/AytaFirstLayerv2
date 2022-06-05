9cc324485e6c32c72604d669fa5355e125c3c836
Muhammad Samin Hasan <53146728+saminhasan@users.noreply.github.com>
2022-06-05 10:50:04 +0400

    read_voltage, gain : 1 -> 2/3

--- ads115.py
+++ ads115.py
@@ -3,7 +3,7 @@ from threading import Thread
 from time import time, sleep
 
 class ADC:
-	def __init__(self, GAIN=1, data_rate=128):
+	def __init__(self, GAIN=2/3, data_rate=128):
 		self.adc = Adafruit_ADS1x15.ADS1115()
 		self.GAIN = GAIN
 		self.data_rate = data_rate
@@ -17,6 +17,20 @@ class ADC:
 			values = [0]*4
 			for i in range(4):
 				# Read the specified ADC channel using the previously set gain value.
+				# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+				# Or pick a different gain to change the range of voltages that are read:
+				#  - 2/3 = +/-6.144V
+				#  -   1 = +/-4.096V
+				#  -   2 = +/-2.048V
+				#  -   4 = +/-1.024V
+				#  -   8 = +/-0.512V
+				#  -  16 = +/-0.256V
+				# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+
+				#This line is calling the read_adc() function from the ADS1x15 Python library.  
+				#The function takes one parameter, the channel number to read (a value of 0 to 3), 
+				#and optionally a gain value (the default is 1). 
+				#As a result the function will return the current ADC value of that channel.
 				values[i] = self.adc.read_adc(i, gain=self.GAIN)
 				# Note you can also pass in an optional data_rate parameter that controls
 				# the ADC conversion time (in samples/second). Each chip has a different
@@ -33,7 +47,16 @@ class ADC:
 			self.adc_data['A3'] = values[3]
 			self.adc_data['adc_timestamp'] = time()
 			sleep(0.05)
-
+			
+	def read_voltage(self, i,gain=self.GAIN):
+		value = self.adc.read_adc(i, gain=self.GAIN)
+		v_lim = self.scale(gain, 1.0, 16.0, 4.096, 0.256)
+		voltage = self.scale(value, -32768, 32767, -v_lim, v_lim)
+		return voltage
+	
+	def scale(self,x, in_min, in_max, out_min, out_max):
+		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+	
 	def get_adc_data(self):
 		adc_data = self.adc_data
 		return adc_data
